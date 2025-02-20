6538ee1c6861c4a134212d53a5d1d957701cb415
sneha4712 <69349569+sneha4712@users.noreply.github.com>
2025-02-20 11:03:33 +0530

    BankAccount.java

--- assignment1/BankAccount.java
+++ assignment1/BankAccount.java
@@ -1,5 +1,6 @@
 package com.celcom.assignment1;
 
+
 import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
@@ -11,81 +12,131 @@ class Account {
 	long phoneNum;
 	double balance;
 	
-	Account(int accountNum, String accHolderName, String branchName,long phoneNum){
-		this.accountNum= accountNum;
+	Account(int accountNum, String accHolderName, String branchName, long phoneNum) {
+		this.accountNum = accountNum;
 		this.accHolderName = accHolderName;
 		this.branchName = branchName;
 		this.phoneNum = phoneNum;
 	}
-	
-		void deposit(int amount) { //To deposit amount
-			balance += amount;
-		}
-		
-		void withDraw(int amount) //To withdraw amount
-		{
+
+	void deposit(float amount) { // To deposit amount
+		balance += amount;
+	}
+
+	void withDraw(float amount) { // To withdraw amount
+		if (amount <= balance) {
 			balance -= amount;
+		} else {
+			System.out.println("Insufficient balance");
 		}
 	}
-class Bank{
-	private Map<Integer,Account> bank = new HashMap<>();
-	Scanner sc = new Scanner(System.in);
-    Account account = new Account(sc.nextInt(), sc.next(), sc.next(), sc.nextLong());
+}
 
-	void deposit(int amount)
-	{
-		account.deposit(amount);
-		System.out.println("Amount deposited successfully");
+class Bank {
+	private Map<Integer, Account> bank = new HashMap<>();
+	private Scanner sc = new Scanner(System.in);
+	
+	
+
+	void createAccount() {
+		System.out.println("Enter Account Number, Account Holder Name, Branch Name, Phone Number: ");
+		int accountNum = sc.nextInt();
+		sc.nextLine();
+		String accHolderName = sc.nextLine();
+		String branchName = sc.nextLine();
+		long phoneNum = sc.nextLong();
+
+		if (bank.containsKey(accountNum)) {
+			System.out.println("Account Already Exists");
+		} else {
+			Account account = new Account(accountNum, accHolderName, branchName, phoneNum);
+			bank.put(accountNum, account);
+			System.out.println("Account Created Successfully");
+		}
 	}
 
-	void withDraw(int amount) {
-		account.withDraw(amount);
+	void deposit(int acc,float amount) {
+		if(bank.containsKey(acc))
+		{
+			Account account = bank.get(acc);
+			account.balance+=amount;
+			System.out.println("Deposited");
+			return;
+		}
+		System.out.println("Invalid Account number");
+	}
+
+	void withDraw(int acc, float amount) {
+		if(bank.containsKey(acc)) {
+			Account account = bank.get(acc);
+			account.balance-=amount;
+			System.out.println("Amount withdrawn successfully");
+			return;
+		}
 		System.out.println("Amount withdrawn successfully");
 	}
-	void setData() {
-		if(bank.containsKey(account.accountNum))
-		{
-			//System.out.println("Account Already Exist");
+
+	void deleteAccount() {
+		System.out.println("Enter Account Number:");
+		int accountNum = sc.nextInt();
+
+		if (bank.containsKey(accountNum)) {
+			bank.remove(accountNum);
+			System.out.println("Account Deleted Successfully");
+		} else {
+			System.out.println("Account Not Found");
 		}
-		else {
-			bank.put(account.accountNum, account);
-		}
-		//System.out.println("Account added sucessfully");
 	}
-
 }
-public class BankAccount {
 
+public class BankAccount {
+int a;
 	public static void main(String[] args) {
-		System.out.println("Enter your Account number: ,  AccountHolder Name: , BranchName: , PhoneNumber: ");
+
 		Scanner sc = new Scanner(System.in);
+		
 		Bank bank = new Bank();
-		while(true) {
+
+		while (true) {
+
+			// bank.setData();
+			// System.out.println("Enter your Account number: , AccountHolder Name: ,
+			// BranchName: , PhoneNumber: ");
 			System.out.println("Please enter your choice");
 			System.out.println(" 1.Deposit");
 			System.out.println(" 2.Withdraw");
-			System.out.println(" 3.Exit"); 
+			System.out.println(" 3.ADD Account");
+			System.out.println(" 4.Delete Account");
+			System.out.println(" 5.Exit");
 			int choice = sc.nextInt();
-			bank.setData();
-			
-			switch(choice) {
+
+			switch (choice) {
 			case 1:
+				System.out.println("Enter the account number");
+				int acc = sc.nextInt();
 				System.out.println("Please Enter the Deposit Amount");
-				bank.deposit(sc.nextInt());
+				bank.deposit(acc,sc.nextFloat());
 				break;
 			case 2:
+				System.out.println("Enter the account number");
+				int acc1 = sc.nextInt();
 				System.out.println("Please Enter the Amount for Withdrawal");
-				bank.withDraw(sc.nextInt());
+				bank.withDraw(acc1,sc.nextFloat());
 				break;
 			case 3:
+				bank.createAccount();
+				break;
+			case 4:
+				bank.deleteAccount();
+				break;
+			case 5:
 				System.out.println("Thank you Visit Again:)");
 				System.exit(0);
 			default:
 				System.out.println("Please Enter a Valid Option");
 			}
-
 		}
- 
+
 	}
- 
+
 }
